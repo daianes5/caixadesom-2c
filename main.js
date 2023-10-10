6633f31e3c0481c3ac753545e7a495872eb72017
daianes5 <105429766+daianes5@users.noreply.github.com>
2023-10-10 09:13:03 -0300

    clap

--- main.js
+++ main.js
@@ -2,3 +2,8 @@ function tocaSomPom(){
     document.querySelector('#som_tecla_pom').play()    
 }
 document.querySelector('.tecla_pom').onclik = tocaSomPom;
+
+function tocaSomClap(){
+    document.querySelector('#som_tecla_clap').play()    
+}
+document.querySelector('.tecla_clap').onclik = tocaSomClap;
