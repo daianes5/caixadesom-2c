1f341cb3b61327e49187194ef9ea59f66cb32110
daianes5 <105429766+daianes5@users.noreply.github.com>
2023-10-24 09:33:27 -0300

    contador

--- main.js
+++ main.js
@@ -1,18 +1,23 @@
 //declaração/ crinado função tocaSomPom
-function tocaSomPom(){
-    document.querySelector('#som_tecla_pom').play()    
+function tocaSom(){
+    document.querySelector(idElementoAudio).play()    
 }
 
 //chamando/invocando a função tocaSomPom pelo clique do botão
-document.querySelector('.tecla_pom').onclik = tocaSomPom;
+document.querySelector('.tecla_pom').onclik = tocaSom;
 
 //referência constante listaDeTecla, buscando todos os botões
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
 //chamando a função tocaSomPom pela item "0" da listaDeTecla
-listaDeTeclas[0].onclick = tocaSomPom;
+listaDeTeclas[0].onclick = tocaSom;
 
 let contador = 0;
-while(){
-    
+while(contador < listaDeTeclas.length){
+    const wfeito =listaDeTeclas[contador].classList[1];
+    const idAudio = "#om_"+efeito;
+    listaDeTeclas[contador].onclick = function(){
+    tocaSom(Audio);
+    contador = contador = +1;
+    console.log(contador) ;
 }
