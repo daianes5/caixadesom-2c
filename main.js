b976b01e3cbcbf5022327ee2feec0ae0883833d2
daianes5 <105429766+daianes5@users.noreply.github.com>
2023-11-07 09:29:16 -0300

    laço "for"

--- main.js
+++ main.js
@@ -12,12 +12,13 @@ const listaDeTeclas = document.querySelectorAll('.tecla');
 //chamando a função tocaSomPom pela item "0" da listaDeTecla
 listaDeTeclas[0].onclick = tocaSom;
 
-let contador = 0;
-while(contador < listaDeTeclas.length){
-    const wfeito =listaDeTeclas[contador].classList[1];
-    const idAudio = "#om_"+efeito;
-    listaDeTeclas[contador].onclick = function(){
-    tocaSom(Audio);
-    contador = contador = +1;
-    console.log(contador) ;
+for( let contador = 0 contador < listaDeTeclas.length;contador++){
+    const tecla = listaDeTeclas[contados}]
+    const efeito =tecla.classList[1];
+    const idAudio = `#som_${efeito}`;
+    tecla.onclick = function(){
+        tocaSom(idAudio);
+    }
+    contador = contador + 1;
+
 }
